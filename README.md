d22e22d169d3f6c8439bc3735fc2f5ec5bee9a33
Anatoliy Yakshov <anatoliy.yakshov@abbyy.com>
2020-04-22 14:10:57 +0300

    hjk

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Hui
+# Build
+
+Используйте make в качестве генератора!
+ 
+``` sh
+mkdir Build
+cd Build
+
+
+cmake <path_to_src> -DCMAKE_BUILD_TYPE=Release
+cmake --build .
+```
--- cmake/Settings.cmake
+++ cmake/Settings.cmake
@@ -84,35 +84,35 @@ function(configure_target TARGET_NAME)
     endif()
 endfunction()
 
-    function(fine_unexport_symbols TARGET_NAME)
-        get_target_property(TARGET_TYPE ${TARGET_NAME} TYPE)
-        if(TARGET_TYPE STREQUAL "SHARED_LIBRARY")
-            if(ANDROID OR LINUX)
-                if(NOT CMAKE_GENERATOR STREQUAL "Unix Makefiles")
-                    message(FATAL_ERROR "Cmake generator must be 'Unix Makefiles'!")
-                endif()
-                
-                if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux|Darwin")
-                    set(LINK_CMD_FILE link.txt)
-                elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
-                    set(LINK_CMD_FILE build.make)
-                else()
-                    message(FATAL_ERROR "Unknown host system!")
-                endif()
-
-                add_custom_command(TARGET ${PROJECT_NAME} 
-                    POST_BUILD
-                    COMMAND python ${FINE_ROOT}/FineObjects/Cmake/unexport.py
-                        lib${PROJECT_NAME}.so ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols.txt 
-                        ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}.dir/${LINK_CMD_FILE}
-                        COMMENT "Unexport symbols in ${PROJECT_NAME}"
-                )
-            elseif(DARWIN)
-                target_link_options(${TARGET_NAME} PRIVATE -unexported_symbols_list ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols_ios.txt)
-            elseif(IOS)
-                set_target_properties(${TARGET_NAME} PROPERTIES
-                    XCODE_ATTRIBUTE_UNEXPORTED_SYMBOLS_FILE ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols_ios.txt
-                )
+function(fine_unexport_symbols TARGET_NAME)
+    get_target_property(TARGET_TYPE ${TARGET_NAME} TYPE)
+    if(TARGET_TYPE STREQUAL "SHARED_LIBRARY")
+        if(ANDROID OR LINUX)
+            if(NOT CMAKE_GENERATOR STREQUAL "Unix Makefiles")
+                message(FATAL_ERROR "Cmake generator must be 'Unix Makefiles'!")
             endif()
+                
+            if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux|Darwin")
+                set(LINK_CMD_FILE link.txt)
+            elseif(CMAKE_HOST_SYSTEM_NAME MATCHES "Windows")
+                set(LINK_CMD_FILE build.make)
+            else()
+                message(FATAL_ERROR "Unknown host system!")
+            endif()
+
+            add_custom_command(TARGET ${PROJECT_NAME} 
+                POST_BUILD
+                COMMAND python ${FINE_ROOT}/FineObjects/Cmake/unexport.py
+                    lib${PROJECT_NAME}.so ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols.txt 
+                    ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${PROJECT_NAME}.dir/${LINK_CMD_FILE}
+                    COMMENT "Unexport symbols in ${PROJECT_NAME}"
+            )
+        elseif(DARWIN)
+            target_link_options(${TARGET_NAME} PRIVATE -unexported_symbols_list ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols_ios.txt)
+        elseif(IOS)
+            set_target_properties(${TARGET_NAME} PROPERTIES
+                XCODE_ATTRIBUTE_UNEXPORTED_SYMBOLS_FILE ${FINE_ROOT}/FineObjects/Cmake/unexported_symbols_ios.txt
+            )
         endif()
-    endfunction()
+    endif()
+endfunction()
